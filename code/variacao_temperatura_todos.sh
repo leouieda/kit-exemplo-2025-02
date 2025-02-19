b4402e646c871040319ab193ea7604a37539ba4b
Leonardo Uieda <leo@uieda.com>
2025-02-19 11:46:16 -0300

    Cria script que calcula variação para todos
    Script usa o script Python e um for para calcular a variação
    de temperatura para todos os países.

---
+++ code/variacao_temperatura_todos.sh
@@ -0,0 +1,8 @@
+# Calcula a variação de temperatura para todos os países
+
+echo "variacao_C_por_ano,pais"
+
+for arquivo in dados/temperatura/a*.csv
+do 
+    python $(dirname $0)/variacao_temperatura.py $arquivo
+done
